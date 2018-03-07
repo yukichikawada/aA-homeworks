55fe2baa436c0ec60a29a0dc826111e307942cb2
Yukichi Kawada <yukichikawada@gmail.com>
2018-03-07 07:19:57 -0800

    Include BCrypt in Gemfile

--- w4d3/rails_auth/app/models/user.rb
+++ w4d3/rails_auth/app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6, allow_nil: true }
   before_validation :ensure_session_token
 
+  attr_reader :password
+
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     if user && BCrypt::Password.new(user.password_digest).is_password?(password)
