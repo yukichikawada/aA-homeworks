af4a0be78dff798a7a7680e769c8a0363f317a47
Yukichi Kawada <yukichikawada@gmail.com>
2018-03-15 07:38:21 -0700

    Skeleton of functions

---
+++ w5d4/phase02.js
@@ -0,0 +1,18 @@
+// Phase i: Callbacks
+
+function titleize(arrayNames, callback) {
+  // Array.prototype.map to create new array
+  // returns 'Mx. arrayNames[0] Jingleheimer Schmidt'
+  // pass to callback which prints with Array.prototype.forEach
+}
+
+// Phase II: constructors, Prototypes, this
+
+// Elephant.prototype.trumpet: should print"(name) the elephant goes 'phrRRRRRRRRRRR!!!!!!!'"
+// Elephant.prototype.grow add 12 inch to height
+// Elephant.prototype.addTrick(trick)
+// Elephant.prototype.play prints random trick
+
+// Phase III: function invocation
+
+// Phase IV: closures
