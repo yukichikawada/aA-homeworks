432bf97d386db283f1cc617782a1dd7a8f6d31c5
Yukichi Kawada <yukichikawada@gmail.com>
2018-03-15 07:30:42 -0700

    Completed second set

--- w5d4/phase01.js
+++ w5d4/phase01.js
@@ -9,3 +9,44 @@ function madLib(verb, adjective, noun) {
 function isSubstring(searchString, substring) {
   return searchString.includes(substring);
 }
+
+// Phase 2
+function fizzBuss(array) {
+  const arr = [];
+
+  array.forEach(el => {
+    if ((el % 3 === 0) ^ (el % 5 === 0)) {
+      arr.push(el);
+    }
+  });
+
+  return arr;
+}
+
+function isPrime(n) {
+  // assume positive
+  if (n < 2) { return false; }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function sumOfNPrimes(n) {
+  // use isPrime
+  let sum = 0;
+  let sumCount = 0;
+
+  for (let i = 2; sumCount < n; i++) {
+    if (isPrime(i)) {
+      sum += i;
+      sumCount++;
+    }
+  }
+
+  return sum;
+}
